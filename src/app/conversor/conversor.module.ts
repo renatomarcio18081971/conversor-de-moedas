f06fc6b1aad0d118815dfe4059d00413d075f579
renatomarcio18081971 <renato.marcio@gmail.com>
2020-04-18 10:58:15 -0300

    nao entendi

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
+import { ModalCotacaoComponent } from './utils';
 
 
 @NgModule({
   declarations: [
     ConversorComponent,
-    NumeroDirective
+    NumeroDirective,
+    ModalCotacaoComponent
   ],
   imports: [
     CommonModule,
